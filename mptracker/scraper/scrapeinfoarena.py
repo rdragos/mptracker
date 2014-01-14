7efe626c63aa8d28fed1273387ebda7f207ccfae
Rotaru Dragos Alin <r.dragos0@gmail.com>
2014-01-14 12:47:28 +0200

    good info

--- mptracker/scraper/scrapeinfoarena.py
+++ mptracker/scraper/scrapeinfoarena.py
@@ -12,7 +12,7 @@ class Infoarena(object):
     problems = [ 
             ('par', 'secv6', 'trompeta', 'cerere'),
             ('tsunami', 'alee', 'amici2'),
-            ('struti'),
+            ['struti'],
             ('adapost2', 'matrice2'),
             ('radio2', 'patrate3', 'map', 'substr'),
             ('berarii2', 'markon', 'reinvent'),
@@ -36,53 +36,66 @@ class Infoarena(object):
             if i == 0:
                 continue
             self.score_set.append(scores.eq(i).text())
-            
-        #from pdb import set_trace; set_trace()
         
-    def getscore(self, problem, user): 
-        user_page = pq('http://www.infoarena.ro/runda/teme_acmunibuc_2013?user='+user) 
-        
-        v = user_page('table.sortable .task a')
-        import pdb;
-        pdb.set_trace();
-        v = list(filter(lambda element : pq(element).attr('href').split('/')[-1] == problem, v))
-        pdb.set_trace()
-        v[0] = pq(v[0]).parent().parent().parent().find('.score').text()
-        
-        pdb.set_trace()
+    def getscore(self, problems, user): 
+        user_page = pq('http://www.infoarena.ro/runda/teme_acmunibuc_2013?user='+user)  
+        results = []
+        for problem in problems:
+            v = user_page('table.sortable .task a') 
+            v = list(
+                    filter(
+                    lambda element: 
+                    pq(element).attr('href').split('/')[-1] == problem, v)
+                )
+            #print (problem, user)
+            v =  pq(v[0]).parent().parent().parent().children().eq(3).text()
+            if v == 'N/A':
+                results.append(0)
+            else:
+                results.append(int(v))
+        return results
 
-        
     def go_scrape(self):
-        """
+        
         self.fetch_users(self.index_url1)
         self.fetch_users(self.index_url2)
-        self.getscore('secv6', 'freak93')
-        """
+        user_set = self.user_set
+        total_problems = self.problems;
 
-        
-        D = dict()
-        total_problems = self.problems
-        
-        for user in user_set:
+        f = open("archive.csv", "w")
+
+        for user_index in range(len(user_set)):
+            if (user_index % 5 == 0):
+                from time import sleep
+                sleep(20)
+
+            user = user_set[user_index]
             v = []
             for problem_group in total_problems:
-                points = 0           
-                for problem in problem_group:
-                    temp_score = getscore(problem, user)
+                points = 0
+                scores = zip(problem_group, self.getscore(problem_group, user))
+
+                for problem in scores:
+                    temp_score = problem[1]
                     if temp_score >= 30 and temp_score < 60:
                         points += 5
                     if temp_score > 60:
                         points += 10
-                v.append(points * 2 / 3)
-            v.sort()
+                L = len(problem_group)
+                grade = points / (2.0 * L * 10 / 3.0) 
+                grade *= 10
+                v.append(min([11,grade]))
 
+            v.sort()
+            total_score = 0
             for i in range(1, len(v)):
                 total_score += v[i]
             total_score /= (len(v) - 1)
-            D.update({user: total_score})
+            print (user +"," + str(total_score))
         """
-        import pdb; pdb.set_trace()
         import json
         f = open("dump-result.json", "w")
         json.dump( D, f)
+        """
+        
 
